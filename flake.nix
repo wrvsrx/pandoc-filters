5d94915e3e32b13db040ecb99cea5660be7056fb
wrvsrx <wrvsrx@outlook.com>
2025-02-12 19:04:16 +0800

    chore(env): use forked nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,24 +2,29 @@
   description = "flake template";
 
   inputs = {
-    flake-lock.url = "github:wrvsrx/flake-lock";
-    nixpkgs.follows = "flake-lock/nixpkgs";
-    flake-parts.follows = "flake-lock/flake-parts";
+    nixpkgs.url = "github:wrvsrx/nixpkgs/patched-nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [ "x86_64-linux" ];
-    flake = {
-      overlays.default = final: prev: (import ./filters { pkgs = prev; }).packages;
-    };
-    perSystem = { pkgs, ... }:
-      let
-        filters = import ./filters { inherit pkgs; };
-      in
-      {
-        packages = filters.packages;
-        devShells = filters.devShells;
-        formatter = pkgs.nixpkgs-fmt;
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      flake = {
+        overlays.default = final: prev: (import ./filters { pkgs = prev; }).packages;
       };
-  };
+      perSystem =
+        { pkgs, ... }:
+        let
+          filters = import ./filters { inherit pkgs; };
+        in
+        {
+          packages = filters.packages;
+          devShells = filters.devShells;
+          formatter = pkgs.nixpkgs-fmt;
+        };
+    };
 }
